55c8a6c6143214a43440553dcbefcba6a57a0052
Nfytxrf <32553700+Nfytxrf@users.noreply.github.com>
2020-03-26 22:23:53 +0300

    Add files via upload
    case 2

---
+++ game.py
@@ -0,0 +1,16 @@
+def game_core_v3(number):
+    import numpy as np
+    count=0 # zero count
+    n1=1 #begin of range
+    n2=101 # end of range
+    predict = np.random.randint(1,100) # made a number
+    while number != predict:
+        number=(n1+n2) // 2 # count middle of range
+        count+=1
+        if number>predict:
+            n2=number  # change range
+        else:
+            n1=number # change range
+    return(count)
+ 
+score_game(game_core_v3)
